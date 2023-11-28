5e3dd3b089f0a319d3ca1ab437b15836e52552bf
ivanprovci <ivanprovci@yahoo.com>
2023-11-28 13:49:58 -0800

    created components folder, minor file restructuring

---
+++ pages/user-profile.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once('../components/head.php'); ?>
+    <title>Profile</title>
+</head>
+
+<body>
+    <?php require_once('../components/nav.php'); ?>
+    <?php echo 'USER-PROFILE'; ?>
+</body>
+
+</html>
